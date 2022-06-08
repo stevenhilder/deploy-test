cdcbf9d0632bae7fe61a3f28f1e3e00145e7411a
Steve Hilder <stevenhilder@users.noreply.github.com>
2022-06-09 01:55:37 +0300

    Twentieth commit

--- game-server/server.ts
+++ game-server/server.ts
@@ -1,31 +1,56 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const app = express();
-const port: number = 80;
+((async () => {
+	try {
 
-const deployment_ini: string = fs.readFileSync(path.resolve(__dirname, '..', '..', '.container', 'deployment.ini'), 'utf8');
+		// Node.js built-in modules:
+		import fs from 'node:fs';
+		import path from 'node:path';
 
-app.get('/', (request: any, response: any) => {
-	response.send(`Hello, World!\n\n${ deployment_ini }`);
-});
+		// NPM dependencies:
+		import createApplication from 'express';
 
-const server = app.listen(port, () => {
-	process.stderr.write(`HTTP server listening on port ${ port }...\n`);
-});
+		const application: Application = createApplication();
+		const port: number = 80;
 
-const shutdown = (signal: string) => () => {
-	process.stderr.write(`\r${ signal } received: closing HTTP server...\n`);
-	setTimeout(() => {
-		server.close(() => {
-			process.stderr.write('...HTTP server closed.\n');
+		const deployment_info: string = fs.readFileSync(path.resolve(__dirname, '..', '..', '.oci', 'deployment.json'), 'utf8');
+
+		application.get('/', (request: Request, response: Response) => {
+			const body: string = `Hello, World!\n\n${ JSON.stringify(JSON.parse(deployment_info), null, '\t') }`;
+			response.writeHead(200, {
+				'Connection': 'close',
+				'Content-Length': Buffer.byteLength(body.length, 'utf8'),
+				'Content-Type': 'text/plain; charset=UTF-8',
+			});
+			response.send(body);
 		});
-	}, 10);
-};
 
-process.on('SIGINT', shutdown('SIGINT'));
-process.on('SIGTERM', shutdown('SIGTERM'));
-app.get('/shutdown', (request: any, response: any) => {
-	response.send('Shutting down...\n');
-	shutdown('Shutdown request');
-});
+		const server = app.listen(port, () => {
+			process.stderr.write(`HTTP server listening on port ${ port }...\n`);
+		});
+
+		const shutdown: () => void = (signal: string) => () => {
+			process.stderr.write(`\r${ signal } received: closing HTTP server...\n`);
+			setTimeout(() => {
+				server.close(() => {
+					process.stderr.write('...HTTP server closed.\n');
+				});
+			}, 10);
+		};
+
+		process.on('SIGINT', shutdown('SIGINT'));
+		process.on('SIGTERM', shutdown('SIGTERM'));
+		app.get('/shutdown', (request: Request, response: Response) => {
+			const body: string = 'Shutting down...';
+			response.writeHead(200, {
+				'Connection': 'close',
+				'Content-Length': Buffer.byteLength(body.length, 'utf8'),
+				'Content-Type': 'text/plain; charset=UTF-8',
+			});
+			response.send(body);
+			setTimeout(() => shutdown('Shutdown request'), 10);
+		});
+	} catch (error: Error | string) {
+		process.stderr.write(`${ error instanceof Error ? error.message : error }\n`);
+	} finally {
+		process.exit(1);
+	}
+}))();
